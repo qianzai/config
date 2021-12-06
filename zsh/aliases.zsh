28be7a23e80c01917d1d7724f0121de4ed0c6a11
Bzm <mail@bzm.ink>
2021-12-06 11:11:35 +0800

    first commit

---
+++ zsh/aliases.zsh
@@ -0,0 +1,12 @@
+# alias
+
+alias s='neofetch'
+alias c='clear'
+alias w='curl wttr.in/Guangzhou'
+alias vim='nvim'
+alias lg='sudo lazygit'
+alias la='ls -la'
+alias ra='ranger'
+alias dh='dunstctl history-pop'
+alias dc='dunstctl close-all'
+alias yc='~/scripts/remote_connection.sh'
